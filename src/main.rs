10a15213cfd9b5b7c6ac6d07ec59783123abd1ef
Lucien Greathouse <me@lpghatguy.com>
2019-10-15 15:11:25 -0700

    Pack together learnings from bash scripts into tool to upload images

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,62 @@
 mod auth_cookie;
 mod roblox_web_api;
 
+use std::{fs, path::PathBuf};
+
 use structopt::StructOpt;
 
+use crate::{
+    auth_cookie::get_auth_cookie,
+    roblox_web_api::{ImageUploadData, RobloxApiClient},
+};
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "A tool to help manage Roblox assets from the command line")]
+struct Options {
+    #[structopt(subcommand)]
+    command: Subcommand,
+}
+
+#[derive(Debug, StructOpt)]
+enum Subcommand {
+    /// Upload a single image to Roblox.com. Prints the asset ID of the resulting
+    /// Image asset to stdout.
+    UploadImage(UploadImage),
+}
+
+#[derive(Debug, StructOpt)]
+struct UploadImage {
+    /// The path to the image to upload.
+    path: PathBuf,
+
+    /// The name to give to the resulting Decal asset.
+    #[structopt(long)]
+    name: String,
+
+    /// The description to give to the resulting Decal asset.
+    #[structopt(long, default_value = "Uploaded by Tarmac.")]
+    description: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let options = Options::from_args();
+
+    match &options.command {
+        Subcommand::UploadImage(upload_options) => {
+            let auth_cookie = get_auth_cookie().expect("no auth cookie");
+            let mut client = RobloxApiClient::new(auth_cookie);
+
+            let upload_data = ImageUploadData {
+                image_data: fs::read(&upload_options.path).unwrap(),
+                name: &upload_options.name,
+                description: &upload_options.description,
+            };
+
+            let response = client.upload_image(upload_data).expect("request failed");
+
+            println!("{}", response.backing_asset_id);
+        }
+    }
 }
--- src/roblox_web_api.rs
+++ src/roblox_web_api.rs
@@ -1,32 +1,145 @@
-use std::path::Path;
+use std::{fs, path::Path};
 
-use crate::auth_cookie::get_auth_cookie;
+use reqwest::{
+    header::{HeaderValue, COOKIE},
+    multipart::{Form, Part},
+    Client, Request, Response, StatusCode,
+};
+use serde::{Deserialize, Serialize};
 
-pub fn upload_image(path: &Path) {
-    // let cookie = get_auth_cookie().expect("no auth cookie");
-    // let url = format!(
-    //     "https://data.roblox.com/Data/Upload.ashx?assetid=0",
-    //     asset_id
-    // );
+#[derive(Debug, Clone)]
+pub struct ImageUploadData<'a> {
+    pub image_data: Vec<u8>,
+    pub name: &'a str,
+    pub description: &'a str,
+}
 
-    // let buffer = fs::read(path).expect("couldn't read file");
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct UploadResponse {
+    pub success: bool,
+    pub asset_id: u64,
+    pub backing_asset_id: u64,
+}
 
-    // let client = reqwest::Client::new();
-    // let response = client
-    //     .get(&url)
-    //     .header(COOKIE, format!(".ROBLOSECURITY={}", auth_cookie))
-    //     .header(CONTENT_TYPE, "application/xml")
-    //     .header(USER_AGENT, "Roblox/WinInet")
-    //     .body(buffer)
-    //     .send()
-    //     .map_err(rlua::Error::external)?;
+pub struct RobloxApiClient {
+    auth_token: String,
+    csrf_token: Option<HeaderValue>,
+    client: Client,
+}
 
-    // if response.status().is_success() {
-    //     Ok(())
-    // } else {
-    //     Err(rlua::Error::external(format!(
-    //         "Roblox API returned an error, status {}.",
-    //         response.status()
-    //     )))
-    // }
+impl RobloxApiClient {
+    pub fn new(auth_token: String) -> Self {
+        Self {
+            auth_token,
+            csrf_token: None,
+            client: Client::new(),
+        }
+    }
+
+    pub fn upload_image(&mut self, data: ImageUploadData) -> reqwest::Result<UploadResponse> {
+        let url = "https://data.roblox.com/data/upload/json?assetTypeId=13";
+
+        let mut response = self.execute_with_csrf_retry(|client| {
+            client
+                .post(url)
+                .query(&[("name", data.name), ("description", data.description)])
+                .body(data.image_data.clone())
+                .build()
+        })?;
+
+        if response.status().is_success() {
+            let body: UploadResponse = response.json()?;
+            Ok(body)
+        } else {
+            let body = response.text().unwrap();
+
+            log::error!("response: {:?}", response);
+            log::error!("status: {:?}", response.status());
+            log::error!("body: {}", body);
+
+            unimplemented!("Handle bad responses");
+        }
+    }
+
+    fn execute_with_csrf_retry<F>(&mut self, make_request: F) -> reqwest::Result<Response>
+    where
+        F: Fn(&Client) -> reqwest::Result<Request>,
+    {
+        let mut request = make_request(&self.client)?;
+        self.attach_headers(&mut request);
+
+        let response = self.client.execute(request)?;
+
+        match response.status() {
+            StatusCode::FORBIDDEN => {
+                if let Some(csrf) = response.headers().get("X-CSRF-Token") {
+                    log::debug!("Retrying request with X-CSRF-Token...");
+
+                    self.csrf_token = Some(csrf.clone());
+
+                    let mut new_request = make_request(&self.client)?;
+                    self.attach_headers(&mut new_request);
+
+                    self.client.execute(new_request)
+                } else {
+                    Ok(response)
+                }
+            }
+            _ => Ok(response),
+        }
+    }
+
+    fn attach_headers(&self, request: &mut Request) {
+        let cookie_value = format!(".ROBLOSECURITY={}", self.auth_token);
+
+        request.headers_mut().insert(
+            COOKIE,
+            HeaderValue::from_bytes(cookie_value.as_bytes()).unwrap(),
+        );
+
+        if let Some(csrf) = &self.csrf_token {
+            request.headers_mut().insert("X-CSRF-Token", csrf.clone());
+        }
+    }
+
+    /// I think this method is supposed to work, but currently does not upload
+    /// assets of type Image correctly.
+    // TODO: Switch to using this endpoint instead if it can work for us.
+    #[allow(dead_code)]
+    fn upload_image_publish_api(&mut self, path: &Path) {
+        let url = "https://publish.roblox.com/v1/assets/upload";
+
+        let mut response = self
+            .execute_with_csrf_retry(|client| {
+                let config = Part::text(
+                    r#"
+                {
+                  "apple": {
+                    "description": "I tried.",
+                    "name": "Apple",
+                    "type": "Image"
+                  }
+                }"#,
+                )
+                .file_name("config.json")
+                .mime_str("application/json")?;
+
+                let buffer = fs::read(path).expect("Couldn't read file");
+                let apple = Part::bytes(buffer)
+                    .file_name("apple.png")
+                    .mime_str("image/png")?;
+
+                let form = Form::new().part("config", config).part("apple", apple);
+
+                client.post(url).multipart(form).build()
+            })
+            .unwrap();
+
+        let body = response.text().unwrap();
+
+        println!("response: {:?}", response);
+        println!("status: {:?}", response.status());
+        println!("body: {}", body);
+    }
 }
