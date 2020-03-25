262f02f5bae41ca4bdcc3358f210385c315ee03a
Lucien Greathouse <me@lpghatguy.com>
2020-03-24 20:56:22 -0700

    Upgrade panic handling and default log configuration

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,14 @@ mod options;
 mod roblox_web_api;
 mod sync_backend;
 
-use std::{error::Error, process};
+use std::{env, panic, process};
 
+use backtrace::Backtrace;
 use structopt::StructOpt;
 
 use crate::options::{Options, Subcommand};
 
-fn run(options: Options) -> Result<(), Box<dyn Error>> {
+fn run(options: Options) -> Result<(), anyhow::Error> {
     match options.command {
         Subcommand::UploadImage(upload_options) => {
             commands::upload_image(options.global, upload_options);
@@ -33,45 +34,72 @@ fn run(options: Options) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    let options = Options::from_args();
-
-    {
-        let log_filter = match options.global.verbosity {
-            0 => "warn",
-            1 => "warn,tarmac=info",
-            2 => "warn,tarmac=debug",
-            3 => "warn,tarmac=trace",
-            _ => "trace",
-        };
-
-        let log_env = env_logger::Env::default().default_filter_or(log_filter);
-
-        env_logger::Builder::from_env(log_env)
-            .format_timestamp(None)
-            .init();
-    }
-
-    let panic_result = std::panic::catch_unwind(|| {
-        if let Err(err) = run(options) {
-            eprintln!("Error: {}", err);
-            process::exit(1);
-        }
-    });
-
-    if let Err(error) = panic_result {
-        let message = match error.downcast_ref::<&str>() {
-            Some(message) => message.to_string(),
-            None => match error.downcast_ref::<String>() {
+    panic::set_hook(Box::new(|panic_info| {
+        // PanicInfo's payload is usually a &'static str or String.
+        // See: https://doc.rust-lang.org/beta/std/panic/struct.PanicInfo.html#method.payload
+        let message = match panic_info.payload().downcast_ref::<&str>() {
+            Some(&message) => message.to_string(),
+            None => match panic_info.payload().downcast_ref::<String>() {
                 Some(message) => message.clone(),
                 None => "<no message>".to_string(),
             },
         };
 
-        show_crash_message(&message);
-        process::exit(2);
-    }
-}
+        log::error!("Tarmac crashed!");
+        log::error!("This is probably a Tarmac bug.");
+        log::error!("");
+        log::error!(
+            "Please consider filing an issue: {}/issues",
+            env!("CARGO_PKG_REPOSITORY")
+        );
+        log::error!("");
+        log::error!("If you can reproduce this crash, try adding the -v, -vv, or -vvv flags.");
+        log::error!("This might give you more information to figure out what went wrong!");
+        log::error!("");
+        log::error!("Details: {}", message);
 
-fn show_crash_message(message: &str) {
-    eprintln!(include_str!("crash-message.txt"), message);
+        if let Some(location) = panic_info.location() {
+            log::error!("in file {} on line {}", location.file(), location.line());
+        }
+
+        // When using the backtrace crate, we need to check the RUST_BACKTRACE
+        // environment variable ourselves. Once we switch to the (currently
+        // unstable) std::backtrace module, we won't need to do this anymore.
+        let should_backtrace = env::var("RUST_BACKTRACE")
+            .map(|var| var == "1")
+            .unwrap_or(false);
+
+        if should_backtrace {
+            eprintln!("{:?}", Backtrace::new());
+        } else {
+            eprintln!(
+                "note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace."
+            );
+        }
+
+        process::exit(1);
+    }));
+
+    let options = Options::from_args();
+
+    let log_filter = match options.global.verbosity {
+        0 => "info",
+        1 => "info,tarmac=debug",
+        2 => "info,tarmac=trace",
+        _ => "trace",
+    };
+
+    let log_env = env_logger::Env::default().default_filter_or(log_filter);
+
+    env_logger::Builder::from_env(log_env)
+        .format_module_path(false)
+        .format_timestamp(None)
+        // Indent following lines equal to the log level label, like `[ERROR] `
+        .format_indent(Some(8))
+        .init();
+
+    if let Err(err) = run(options) {
+        log::error!("{:?}", err);
+        process::exit(1);
+    }
 }
